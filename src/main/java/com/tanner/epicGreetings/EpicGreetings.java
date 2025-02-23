c11cb0cea3166b343e54ff277e3ebdc34d593026
Tanner <tannerelephant@gmail.com>
2025-02-23 08:02:06 -0800

    add custom join message

--- src/main/java/com/tanner/epicGreetings/EpicGreetings.java
+++ src/main/java/com/tanner/epicGreetings/EpicGreetings.java
@@ -1,5 +1,6 @@
 package com.tanner.epicGreetings;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EpicGreetings extends JavaPlugin {
@@ -7,7 +8,8 @@ public final class EpicGreetings extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        saveDefaultConfig();
+        Bukkit.getPluginManager().registerEvents(new EpicGreetingsListener(this), this);
     }
 
     @Override
--- src/main/java/com/tanner/epicGreetings/EpicGreetingsListener.java
+++ src/main/java/com/tanner/epicGreetings/EpicGreetingsListener.java
@@ -1,2 +1,35 @@
-package com.tanner.epicGreetings;public class EpicGreetingsListener {
+package com.tanner.epicGreetings;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class EpicGreetingsListener implements Listener {
+
+    private FileConfiguration config;
+
+    public EpicGreetingsListener(EpicGreetings epicGreetings) {
+        config = epicGreetings.getConfig();
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+
+        String joinMessage = convertConfigMessage(config.getString("join-message"), player);
+        Bukkit.broadcastMessage(joinMessage);
+
+        player.playSound(player, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+    }
+
+    private String convertConfigMessage(String joinMessage, Player player) {
+        joinMessage = joinMessage.replace("{player}", player.getDisplayName());
+        joinMessage = ChatColor.translateAlternateColorCodes('&', joinMessage);
+        return joinMessage;
+    }
 }
